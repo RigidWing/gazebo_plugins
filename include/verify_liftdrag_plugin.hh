1556a6d4f8f6ddff8a1f26f5623c6f0317865ad2
Isabelle El-Hajj <isabelle.elhajj@hotmail.com>
2018-12-09 11:45:47 +0100

    created a plugin that gives different wind conditions and logs the forces obtained.

---
+++ include/verify_liftdrag_plugin.hh
@@ -0,0 +1,102 @@
+#ifndef _GAZEBO_VERIFY_LIFTDRAG_PLUGIN_HH_
+#define _GAZEBO_VERIFY_LIFTDRAG_PLUGIN_HH_
+
+#include <string>
+#include <vector>
+#include "gazebo/common/Plugin.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/transport/TransportTypes.hh"
+#include <ignition/math.hh>
+
+// KITEPOWER (Xander)
+#include "common.h"
+#include "common.h"
+#include <common.h>
+#include "WindField.pb.h"
+
+#include <iostream>
+#include <fstream>
+// #include <stdlib.h>     /* atof */
+#include <string>
+#include <list>
+#include <vector>
+#include <ignition/math/Vector3.hh>
+#include <gtest/gtest.h>
+
+
+#include "send_protobuf_msgs.hh"
+
+#include <math.h>
+// #include <process.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+namespace gazebo
+{
+ class GAZEBO_VISIBLE VerifyLiftdragPlugin : public ModelPlugin
+ {
+
+ public: VerifyLiftdragPlugin();
+ public: ~VerifyLiftdragPlugin();
+
+  public:
+    int type_of_test;
+    int wind_condition_iterator;
+    int wind_force_status_iterator;
+
+    double start;
+    double end;
+    double step;
+    double second_spawn_arg;
+    double third_spawn_arg;
+
+    double force_X;
+    double force_Y;
+    double force_Z;
+
+    double magnitude;
+    double azimuth;
+    double elevation;
+
+    double control_variable;
+
+    std::string spawn_args[4];
+
+ /// \brief Pointer to model containing plugin.
+ protected: physics::ModelPtr model;
+
+ /// \brief SDF for this plugin;
+ protected: sdf::ElementPtr sdf;
+
+ /// \brief Connection to World Update events.
+ protected: event::ConnectionPtr updateConnection;
+
+ private: transport::SubscriberPtr force_sub_;
+
+ typedef const boost::shared_ptr<const msgs::WrenchStamped> WrenchStampedMsgPtr;
+
+ typedef const boost::shared_ptr<const msgs::Wrench> WrenchMsgPtr;
+
+
+ void ForceMsgCallback(WrenchStampedMsgPtr &wrench_stamped_msg);
+
+ public: msgs::Wrench wrench_msg;
+
+ public: msgs::Vector3d force_msg;
+
+ protected: virtual void OnUpdate();
+ public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+
+ //
+ public: std::string jointName;
+ public: std::string jointNameFull;
+ protected: physics::JointPtr joint;
+
+public: physics::JointWrench jointWrench;
+
+
+};
+}
+
+#endif
---
+++ verify_liftdrag_plugin.cpp
@@ -0,0 +1,218 @@
+#include "verify_liftdrag_plugin.hh"
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include "common.h"
+#include "gazebo/common/Assert.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/sensors/SensorManager.hh"
+#include "gazebo/transport/transport.hh"
+#include "gazebo/msgs/msgs.hh"
+#include "common.h"
+#include <string>
+#include <cstring>
+#include <sstream>
+
+using namespace gazebo;
+
+GZ_REGISTER_MODEL_PLUGIN(VerifyLiftdragPlugin)
+
+VerifyLiftdragPlugin::VerifyLiftdragPlugin()
+{
+  this->wind_condition_iterator = 0;
+}
+
+VerifyLiftdragPlugin::~VerifyLiftdragPlugin()
+{
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void VerifyLiftdragPlugin::Load(physics::ModelPtr _model,sdf::ElementPtr _sdf)
+{
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Load parameters from the SDF //////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  // Type of test:
+  // 0:Change azimuth
+  // 1:Change elevation
+  // 2:Change magnitude
+
+  GZ_ASSERT(_sdf->Get<int>("type_of_test"),"The test type should be specified.");
+
+
+
+  this->type_of_test = _sdf->Get<int>("type_of_test");
+
+  if(this->type_of_test == 0)
+  {
+    std::cout << "Azimuth test" << std::endl;
+    this->start = _sdf->Get<double>("start_azimuth");
+    this->end = _sdf->Get<double>("end_azimuth");
+    this->step = _sdf->Get<double>("step_azimuth");
+
+    this->second_spawn_arg = _sdf->Get<double>("elevation");
+    this->third_spawn_arg = _sdf->Get<double>("magnitude");
+  }
+  else if(this->type_of_test == 1)
+  {
+    std::cout << "Elevation test" << std::endl;
+    this->start = _sdf->Get<double>("start_elevation");
+    this->end = _sdf->Get<double>("end_elevation");
+    this->step = _sdf->Get<double>("step_elevation");
+
+    this->second_spawn_arg = _sdf->Get<double>("azimuth");
+    this->third_spawn_arg = _sdf->Get<double>("magnitude");
+  }
+  else if(this->type_of_test == 2)
+  {
+    std::cout << "Magnitude test" << std::endl;
+    this->start = _sdf->Get<double>("start_magnitude");
+    this->end = _sdf->Get<double>("end_magnitude");
+    this->step = _sdf->Get<double>("step_magnitude");
+
+    this->second_spawn_arg = _sdf->Get<double>("azimuth");
+    this->third_spawn_arg = _sdf->Get<double>("elevation");
+  }
+  else
+  {
+    gzerr << "The type of test is incorrect. \n";
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Initialize transport //////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  GZ_ASSERT(_model, "VerifyLiftdragPlugin _model pointer is NULL");
+  GZ_ASSERT(_sdf, "VerifyLiftdragPlugin _sdf pointer is NULL");
+  this->model = _model;
+  this->sdf = _sdf;
+
+  if (_sdf->HasElement("joint_name"))
+  {
+    // Get the full name of the joint
+    this->jointName = _sdf->Get<std::string>("joint_name");
+    this->jointNameFull = this->model->GetJoint(jointName)->GetScopedName();
+
+    // get the needed jointptr from the Model (takes as input The name of the joint, specified in the world file)
+    this->joint = this->model->GetJoint(this->jointName);
+    EXPECT_TRUE(this->joint != NULL);
+  }
+  node_handle_ = transport::NodePtr(new transport::Node());
+  node_handle_->Init();
+
+
+  // Get the force from the sensor
+  // force_sub_ = node_handle_->Subscribe<msgs::WrenchStamped>("/gazebo/default/rigid_wing/rigid_wing/fixed_to_world/main_force_torque/wrench",&VerifyLiftdragPlugin::ForceMsgCallback, this);
+  //////////////////////////////////////////////////////////////////////////////
+  //update connection ////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&VerifyLiftdragPlugin::OnUpdate, this));
+
+  // Initialize the control variable
+  this->control_variable = this->start;
+
+  // Create the file needed for logging
+  std::freopen("verify_liftdrag.txt","w", stdout);
+
+}
+
+void VerifyLiftdragPlugin::OnUpdate()
+{
+
+  std::cout << "Onupdate \n";
+  std::cout << "The control variable: " << this->control_variable << std::endl;
+
+  std::cout << "Change of control variable iterator" << std::endl;
+  if(this->type_of_test == 0) // azimuth test
+  {
+    this->spawn_args[0] = std::to_string(this->third_spawn_arg); //magnitude
+    this->spawn_args[1] = std::to_string(this->control_variable); //azimuth
+    this->spawn_args[2] = std::to_string(this->second_spawn_arg); //elevation
+  }
+  else if(this->type_of_test == 1) // elevation test
+  {
+    this->spawn_args[0] = std::to_string(this->third_spawn_arg); //magnitude
+    this->spawn_args[1] = std::to_string(this->second_spawn_arg); //azimuth
+    this->spawn_args[2] = std::to_string(this->control_variable);//elevation
+  }
+  else // magnitude test
+  {
+    this->spawn_args[0] = std::to_string(this->control_variable); //magnitude
+    this->spawn_args[1] = std::to_string(this->second_spawn_arg); //azimuth
+    this->spawn_args[2] = std::to_string(this->third_spawn_arg); //elevation
+  }
+
+  std::string ss;
+  ss = "./build/send_commandline_protobuf " + spawn_args[0] + " " + spawn_args[1] + " " + spawn_args[2];
+
+  system(ss.c_str());
+
+  this->jointWrench = this->joint->GetForceTorque(0u);
+
+
+  // ignition::math::Vector3d force_body1 = this->jointWrench.body1Force();
+  this->force_X = this->jointWrench.body1Force.X();
+  this->force_Y = this->jointWrench.body1Force.Y();
+  this->force_Z = this->jointWrench.body1Force.Z();
+
+
+  // update the control variable (Note that a wait of two update loops is needed to get to the correct values)
+  if (this->wind_condition_iterator == 2){
+    this->control_variable = this->control_variable + this->step;
+
+    if (this->control_variable > this->end)
+    {
+      this->control_variable = this->start;
+    }
+
+    // Log
+    std::cout << "Iterator: " << this->wind_condition_iterator << std::endl;
+    std::cout << "Magnitude: " << this->spawn_args[0] << std::endl;
+    std::cout << "Azimuth: " << this->spawn_args[1] << std::endl;
+    std::cout << "Elevation: " << this->spawn_args[2] << std::endl;
+    std::cout << "Force x: " << this->force_X << std::endl;
+    std::cout << "Force y: " << this->force_Y << std::endl;
+    std::cout << "Force z: " << this->force_Z << std::endl;
+
+    this->wind_condition_iterator = 0;
+  }
+  else
+  {
+    this->wind_condition_iterator++;
+  }
+
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Force callback function /////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// void VerifyLiftdragPlugin::ForceMsgCallback(WrenchStampedMsgPtr &wrench_stamped_msg){
+//
+//   std::cout << "Inside the callback function " << std::endl;
+//
+//
+//
+//   this->wrench_msg = wrench_stamped_msg->wrench();//this->joint->GetForce(0);
+//   this->force_msg = this->wrench_msg.force();
+//
+//   this->force_X = this->force_msg.x();
+//   this->force_Y = this->force_msg.y();
+//   this->force_Z = this->force_msg.z();
+//
+//   this->wind_force_status_iterator = 1;
+//
+//   // // Log
+//   // std::cout << "Iterator: " << this->wind_condition_iterator << std::endl;
+//   // std::cout << "Magnitude: " << this->spawn_args[0] << std::endl;
+//   // std::cout << "Azimuth: " << this->spawn_args[1] << std::endl;
+//   // std::cout << "Elevation: " << this->spawn_args[2] << std::endl;
+//   // std::cout << "Force x " << this->force_X << std::endl;
+//   // std::cout << "Force y " << this->force_Y << std::endl;
+//   // std::cout << "Force z " << this->force_Z << std::endl;
+//
+// }
